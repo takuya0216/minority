18764efb7cf4c6e70b886e67c136c85b6f61927a
takuya ishida <takuya0034@gmail.com>
2022-05-17 20:55:38 +0900

    スマホ仕様に変更

--- swell_child/myscript.js
+++ swell_child/myscript.js
@@ -14,25 +14,50 @@ window.addEventListener('resize', () => {
 jQuery(function ($) {
 
   const scrollTime = 700;
-
+	$('.fade-in-up').each(function () {
+        if(is_in_sight($(this))){
+			$(this).addClass('effect-scroll');
+		}
+	  });
   /* window scroll function start*/
   $(window).scroll(function () {
     scroll_effect();
-  }).trigger('scoll');
+  })
   /* window scroll function end*/
 
-  /*トップのスクロールボタン矢印*/
+  /* コンタクトフォームsubmit function start*/
+  $('form.wpcf7-form').on('submit', function() {
+	  if($('.wpcf7-not-valid-tip').length == 0){
+		  $('.wpcf7cpcnf-title').css('display', 'block');
+	  }
+	  else{
+		  $('.wpcf7cpcnf-title').css('display', 'none');
+	  }
+  });
+  /* コンタクトフォームsubmit load function end*/
+  $(document).on('click', 'button.wpcf7cp-cfm-edit-btn, button.wpcf7cp-cfm-submit-btn', function(){
+	  $('.wpcf7cpcnf-title').css('display', 'none');
+});
+
+  /*トップメインビジュアル スクロールボタン矢印*/
   $('p-mainVisual__scrollArrow').empty();
   $('.p-mainVisual__scroll').html('<svg class="p-mainVisual__scrollArrow" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 34" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><line x1="11" y1="0" x2="11" y2="29"></line></svg><span class="p-mainVisual__scrollLabel">Scroll</span>');
 
-	/*トップへ戻るボタン スクロール*/
+  /*トップへ戻るボタン スクロール*/
   $('#pagetop').on('click',function(){
       $("html").animate({scrollTop: 0}, { duration: scrollTime, easing: 'swing', });
   });
 
+  /*インスタグラムフィードのボタンテキスト変更*/
+  $('.sbi_btn_text').text("Load More...");
+  /*インスタグラムフィード画像にフェードイン付加*/
+　$('.sbi_item').each(function(){
+	 $(this).addClass('fade-in-up');
+ });
+
   /*日付カレンダー設定*/
   $("#cf7-pickup-date").datepicker({
-    dateFormat: 'yy年mm月dd日',
+      dateFormat: 'yy/mm/dd',
 	  yearSuffix: '年',
 	  showMonthAfterYear: true,
 	  monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
@@ -41,13 +66,13 @@ jQuery(function ($) {
 	  minDate: "+5d",
   });
   $("#Date").datepicker("setDate", "+5d");
-
-  //スクロールアニメーション
+  
+  //スクロールアニメーション
   function scroll_effect() {
     //fade-in-up
     $('.fade-in-up').each(function () {
         //.fade-in-upを指定したエレメントのBottom位置がトリガー。
-        var elemPos = $(this).offset().top + $(this).outerHeight() - 100;
+        var elemPos = $(this).offset().top + $(this).outerHeight()-100;
         var scroll = $(window).scrollTop();
         var windowHeight = $(window).height();
         if (elemPos < scroll + windowHeight){
@@ -55,4 +80,16 @@ jQuery(function ($) {
         }
     });
   }
+  //要素の画面内判定
+　function is_in_sight(jq_obj) {
+	var scroll_top    = $(window).scrollTop();
+	var scroll_bottom = scroll_top + $(window).height();
+	var target_top    = jq_obj.offset().top;
+	var target_bottom = target_top + jq_obj.height();
+    if (scroll_bottom > target_top && scroll_top < target_bottom) {
+    	return true;
+    } else {
+    	return false;
+    }
+ }
 });
