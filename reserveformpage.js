c35d07a9c611e510eb9ed9f98dbc6bf570f7475e
takuya ishida <takuya0034@gmail.com>
2022-06-14 23:12:36 +0900

    予約フォーム修正

--- reserveformpage.js
+++ reserveformpage.js
@@ -1,1 +1,2 @@
- $('label[for="client-adult"]').text('人数(パルフェ数)');
+$('label[for="client-adult"]').text('人数(パルフェ数)');
+$('#booking-confirm-fieldset').find('table').find('tbody').find('tr:nth-child(2)').find('th').text('予約人数(パルフェ数)')
